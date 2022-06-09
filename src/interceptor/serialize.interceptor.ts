8e4bbfb96a8d7ba1ab66f13511efc9a0e9149feb
Seun Funmi <funmi@intelligentinnovations.co>
2022-06-09 17:21:45 +0100

    serialization and interceptors

--- src/interceptor/serialize.interceptor.ts
+++ src/interceptor/serialize.interceptor.ts
@@ -7,16 +7,25 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
-import { UserDto } from '../users/dtos/user.dto';
+
+interface ClassConstructor {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor ) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
   intercept(
     context: ExecutionContext,
     handler: CallHandler<any>,
   ): Observable<any> {
     return handler.handle().pipe(
       map((data: any) => {
-        return plainToClass(UserDto, data, {
+        return plainToClass(this.dto, data, {
           excludeExtraneousValues: true,
         });
       }),
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,9 +13,11 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { updateUserDto } from './dtos/updateUser.dto';
 import { UsersService } from './users.service';
-import { SerializeInterceptor } from '../interceptor/serialize.interceptor';
+import { Serialize } from '../interceptor/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private userService: UsersService) {}
   @Post('/signup')
@@ -23,7 +25,7 @@ export class UsersController {
     this.userService.create(body.email, body.password);
   }
 
-  @UseInterceptors(SerializeInterceptor)
+  // @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.userService.findOne(parseInt(id));
