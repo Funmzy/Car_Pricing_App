8ac4b25b843bd68514a323463f91c69fc09ac2bb
Seun Funmi <funmi@intelligentinnovations.co>
2022-06-14 01:09:24 +0100

    auth-guards

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
-  (data: never, context: ExecutionContext) => {
+  (data: any, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
     return request.currentUser;
   },
